ed5e69686de334d170c56878c823de178968bf76
Turi Balázs <developer@bali0531.hu>
2024-06-11 10:17:25 +0200

    Update log_user_info.php

--- log_user_info.php
+++ log_user_info.php
@@ -1,14 +1,47 @@
 <?php
-// Get the JSON data from the request
+// Retrieve the posted data
 $data = json_decode(file_get_contents('php://input'), true);
 
-// Define the file to store the data
-$data_file = 'user_data.json';
+// Append data to user_data.json
+$file = 'user_data.json';
+if (file_exists($file)) {
+    $currentData = json_decode(file_get_contents($file), true);
+    $currentData[] = $data;
+} else {
+    $currentData = array($data);
+}
+file_put_contents($file, json_encode($currentData, JSON_PRETTY_PRINT));
 
-// Append user info to the file
-file_put_contents($data_file, json_encode($data) . PHP_EOL, FILE_APPEND);
+// Prepare the Discord embed message
+$fields = [];
+foreach ($data as $key => $value) {
+    $fields[] = [
+        'name' => ucfirst(str_replace('_', ' ', $key)),
+        'value' => $value,
+        'inline' => false
+    ];
+}
 
-// Send a response back to the client
-header('Content-Type: application/json');
-echo json_encode(array('status' => 'success'));
+$webhook_url = 'YOUR_DISCORD_WEBHOOK_URL';
+$discord_message = [
+    'embeds' => [
+        [
+            'title' => 'New user data logged',
+            'fields' => $fields,
+            'color' => hexdec('00FF00') // Green color
+        ]
+    ]
+];
+
+$options = [
+    'http' => [
+        'header'  => "Content-type: application/json\r\n",
+        'method'  => 'POST',
+        'content' => json_encode($discord_message),
+    ],
+];
+$context  = stream_context_create($options);
+$result = file_get_contents($webhook_url, false, $context);
+
+echo json_encode(['status' => 'success']);
 ?>
